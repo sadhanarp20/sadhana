3b621d64e1885299bf122402955d1cba4ac4ca26
sadha <sadha@BOOK-9V08B7FMOF>
2025-02-27 11:17:36 +0530

    merge

--- Github/src/git/Main2.java
+++ Github/src/git/Main2.java
@@ -1,7 +1,8 @@
 package git;
 
 public class Main2 {
-public static void main(String[] args) {
-	System.out.println("file 1 merge");
-}
+	public static void main(String[] args) {
+		System.out.println("file Merge");
+	}
+
 }
